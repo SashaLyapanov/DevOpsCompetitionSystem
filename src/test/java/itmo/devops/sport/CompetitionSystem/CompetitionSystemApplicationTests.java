5d6a4bddab119a518d84f2ba3667b98bd401ba7e
SashaLyapanov <lyapanov_s@mail.ru>
2025-03-16 12:24:27 +0300

    InitialCommit

---
+++ src/test/java/itmo/devops/sport/CompetitionSystem/CompetitionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package itmo.devops.sport.CompetitionSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetitionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
